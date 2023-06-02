9decb4919c97bb2f28477cddaa2c40e7d19b7063
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-06-02 19:51:48 +0800

    Update main.tf -remove postgres

--- main.tf
+++ main.tf
@@ -87,36 +87,36 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
 #   profile = "default"
 #   region  = "us-east-1"
 # }
-data "aws_vpc" "default" {
-  default = true
-}
-resource "random_string" "uddin-db-password" {
-  length  = 32
-  upper   = true
-  number  = true
-  special = false
-}
-resource "aws_security_group" "uddin" {
-  vpc_id      = "${data.aws_vpc.default.id}"
-  name        = "uddin"
-  description = "Allow all inbound for Postgres"
-ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_db_instance" "uddin-sameed" {
-  identifier             = "uddin-sameed"
-  name                   = "uddin"
-  instance_class         = "db.t2.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "12.5"
-  skip_final_snapshot    = true
-  publicly_accessible    = true
-  vpc_security_group_ids = [aws_security_group.uddin.id]
-  username               = "sameed"
-  password               = "random_string.uddin-db-password.result}"
-}
+# data "aws_vpc" "default" {
+#   default = true
+# }
+# resource "random_string" "uddin-db-password" {
+#   length  = 32
+#   upper   = true
+#   number  = true
+#   special = false
+# }
+# resource "aws_security_group" "uddin" {
+#   vpc_id      = "${data.aws_vpc.default.id}"
+#   name        = "uddin"
+#   description = "Allow all inbound for Postgres"
+# ingress {
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+# resource "aws_db_instance" "uddin-sameed" {
+#   identifier             = "uddin-sameed"
+#   name                   = "uddin"
+#   instance_class         = "db.t2.micro"
+#   allocated_storage      = 5
+#   engine                 = "postgres"
+#   engine_version         = "12.5"
+#   skip_final_snapshot    = true
+#   publicly_accessible    = true
+#   vpc_security_group_ids = [aws_security_group.uddin.id]
+#   username               = "sameed"
+#   password               = "random_string.uddin-db-password.result}"
+# }
